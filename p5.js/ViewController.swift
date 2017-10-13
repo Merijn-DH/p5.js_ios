b6b291b314ca16a1e5a52139b5ed62f74b85243b
Merijn den Houting <merijndenhouting@MacBook-Pro-van-Merijn.local>
2017-10-13 23:48:57 +0200

    Initial Commit

---
+++ p5.js/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  p5.js
+//
+//  Created by Merijn den Houting on 13-10-17.
+//  Copyright © 2017 Merijn den Houting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ p5.jsTests/p5_jsTests.swift
@@ -0,0 +1,36 @@
+//
+//  p5_jsTests.swift
+//  p5.jsTests
+//
+//  Created by Merijn den Houting on 13-10-17.
+//  Copyright © 2017 Merijn den Houting. All rights reserved.
+//
+
+import XCTest
+@testable import p5_js
+
+class p5_jsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
